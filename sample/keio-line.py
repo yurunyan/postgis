664289ed5e5221b35fe9f0f98957afb3628d1cfb
BuildTools <yurunyan@users.noreply.github.com>
2018-04-16 23:42:03 +0900

    aawojbfdb

--- sample/keio-line.py
+++ sample/keio-line.py
@@ -1,4 +1,7 @@
 import json
+import simplekml
+
+kml = simplekml.Kml()
 
 path = r"G:\data\geography\test\N02-16_GML\N02-16_Station.geojson"
 with open(path, 'r', encoding='utf8') as f:
@@ -6,5 +9,14 @@ with open(path, 'r', encoding='utf8') as f:
 data = json.loads(data)
 for x in data["features"]:
     if x['properties']['運営会社'] == '京王電鉄':
+        sta = x["geometry"]["coordinates"]
+        sta_name = x["properties"]["駅名"]
+        
+        kml.newlinestring(name=sta_name, coords=(sta))
+
         print(x["properties"]["駅名"])
         print(x["geometry"]["coordinates"])
+
+kml.save("keio.kml")
+
+        
