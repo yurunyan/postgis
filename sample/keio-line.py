b81c21d6a2bc5d6f60212979251b594ab89cc726
BuildTools <yurunyan@users.noreply.github.com>
2018-04-17 00:57:42 +0900

    a

--- sample/keio-line.py
+++ sample/keio-line.py
@@ -1,8 +1,9 @@
-import json
+import json, csv
 import simplekml
 import numpy as np
 
 kml = simplekml.Kml()
+lses = []
 
 path = r"G:\data\geography\test\N02-16_GML\N02-16_Station.geojson"
 with open(path, 'r', encoding='utf8') as f:
@@ -12,22 +13,16 @@ for x in data["features"]:
     if x['properties']['運営会社'] == '京王電鉄':
         sta = x["geometry"]["coordinates"]
         sta_name = x["properties"]["駅名"]
-
-        print(sta)
-        
         kml.newlinestring(name=sta_name, coords=sta)
 
         sta2 = np.mean(sta, axis=0).tolist()
-        print(sta2)
-        print(type(sta2))
-
         p = kml.newpoint(name=sta_name)
         p.coords = [(sta2[0], sta2[1])]
 
-        a=[(sta2[0], sta2[1])]
-        print(type(a))
-        print(a)
+        lses.append([sta_name, sta2[0], sta2[1]])
 
 kml.save("keio.kml")
-
-        
+print(lses)
+with open("keio.csv", 'w') as f:
+    ff = csv.writer(f, lineterminator='\n')
+    ff.writerows(lses)
